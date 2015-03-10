027f49838262f4c3cd7aa5df976a252a0cd779e3
Geoffe Elias <geoffe02@maki.local>
2015-03-10 16:41:51 +0800

    modified:   movement_controller.rb 	modified:   ../../../Map_Service/config/routes.rb 	../../db/schema.rb 	../../../Map_Service/app/assets/javascripts/directions.coffee 	../../../Map_Service/app/assets/stylesheets/directions.scss 	../../../Map_Service/app/controllers/directions_controller.rb 	../../../Map_Service/app/helpers/directions_helper.rb 	../../../Map_Service/test/controllers/directions_controller_test.rb

--- GameClient/app/controllers/movement_controller.rb
+++ GameClient/app/controllers/movement_controller.rb
@@ -2,6 +2,7 @@ class MovementController < ApplicationController
   require 'curb'
   require 'json'
 
+#    @location = 0 # note: location serves as a kind of "state"
   def index
     @location = 0 # note: location serves as a kind of "state"
 #    http = Curl.post("http://localhost:3002/map#{@location}")
@@ -14,14 +15,29 @@ class MovementController < ApplicationController
   end
  
   def go_north
+    _present_location = @location
+    http = Curl.post("http://localhost:3002/directions",
+      "{\"present_location\": #{_present_location},\"direction\":\"north" }"
+  
+    server_reply = http.body_str
+    data_hash = JSON.parse(server_reply)
+    _new_location = data_hash['location']
+    _latitude = data_hash['latitude']
+    _longitude = data_hash['longitude']
+    _area_description = data_hash['area']
+    
+    render :index
   end
 
   def go_south
+    render :index
   end
   
   def go_east
+    render :index
   end
  
   def go_west
+    render :index
   end
 end
--- Map_Service/config/routes.rb
+++ Map_Service/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root :to =>"mapspaces#index"
+  resources :directions
   resources :mapspaces do
    collection { post :import }
   end
+  
 
 # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
